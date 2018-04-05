a6fc7a5b6a0b100b18e40c4a76c3b6c0e9b7d142
Zoltán Kovács <zoltan@geogebra.org>
2018-04-05 16:40:51 +0200

    Change project name from "Math Apps" to "GeoGebra Apps", use local links

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
-# GeoGebra Math Apps
-Solve math problems, graph functions, create geometric constructions, do statistics and calculus, save and share your results. All of that in your browser using GeoGebra Math Apps (http://www.geogebra.org/apps).
+# GeoGebra Embedding Examples
+Solve math problems, graph functions, create geometric constructions, do statistics and calculus, save and share your results.
+All of that in your browser using [GeoGebra](http://www.geogebra.org/classic).
 
-# Embedding Math Apps in your website
-With our JavaScript library it's easy to embed GeoGebra in any website, please see the following documentation
-- https://www.geogebra.org/manual/en/Reference:Math_Apps_Embedding -- how to embed and customize the Apps
-- https://www.geogebra.org/manual/en/Reference:JavaScript -- JavaScript API to interact with the Apps
-- https://www.github.com/geogebra/math-apps-examples -- examples of Math Apps embedded in HTML you can clone and play with
+## Embedding GeoGebra in your website
+With GeoGebra's JavaScript library it is easy to embed GeoGebra in any website. You can learn more on this at:
+- [Reference: GeoGebra Apps Embedding](https://www.geogebra.org/manual/en/Reference:GeoGebra_Apps_Embedding)&mdash;how to embed and customize the Apps
+- [Reference: JavaScript](https://www.geogebra.org/manual/en/Reference:JavaScript)&mdash;JavaScript API to interact with the Apps
 
-# License
-You are free to copy, distribute and transmit GeoGebra for non-commercial purposes. For details see https://www.geogebra.org/license
+## License
+You are free to copy, distribute and transmit GeoGebra for non-commercial purposes. For details see [license](https://www.geogebra.org/license).
--- html/navigation.js
+++ html/navigation.js
@@ -2,7 +2,7 @@ function addGithubLink(){
 	var filename = window.location.pathname.split("/").slice(-1)[0];
 var menu =
 '<a href="#" id=drawerToggle class="material-icons">menu</a>'+
-'<div id=githubLink><a href="https://github.com/geogebra/math-apps-examples/blob/master/html/'+filename+'">View on GitHub</a></span>';
+'<div id=githubLink><a href="https://github.com/kovzol/geogebra-embedding-examples/blob/master/html/'+filename+'">View on GitHub</a></span>';
 var menuDiv = document.createElement('div');
 menuDiv.className = "sample-toolbar";
 menuDiv.innerHTML = menu;
@@ -13,22 +13,22 @@ document.body.insertBefore(menuDiv,document.body.children[0]);
 function addDrawer(){
 	var filename = window.location.pathname.split("/").slice(-1)[0];
 	var menu =
-	'<h2>GeoGebra Math Apps</h2><h4>Examples</h4><ul><li> <a href="http://dev.geogebra.org/examples/html/example-single.html">Single applet</a></li>'+
-	'<li> <a href="http://dev.geogebra.org/examples/html/example-3d.html">Single applet 3D</a></li>'+
-	'<li> <a href="http://dev.geogebra.org/examples/html/example-multiple.html">Two applets</a></li>'+
-	'<li> <a href="http://dev.geogebra.org/examples/html/example-api.html">Single applet + API</a></li>'+
-	'<li> <a href="http://dev.geogebra.org/examples/html/example-api-multiple.html">Two applets + API</a></li>'+
-	'<li> <a href="http://dev.geogebra.org/examples/html/example-api-save-state.html">Single applet + API for saving</a></li>'+
-    '<li> <a href="http://dev.geogebra.org/examples/html/example-graphing.html">Graphing Calculator with Toolbar</a></li>'+
-    '<li> <a href="http://dev.geogebra.org/examples/html/example-geometry.html">Geometry with Toolbar</a></li>'+
-    '<li> <a href="http://dev.geogebra.org/examples/html/example-tools.html">GeoGebra Classic with Toolbar</a></li>'+
-    '<li> <a href="http://dev.geogebra.org/examples/html/example-popup-graphing.html">Graphing Calculator in a Popup Dialog</a></li>'+
-    '<li> <a href="http://dev.geogebra.org/examples/html/example-popup-geometry.html">Geometry in a Popup Dialog</a></li>'+
-    '<li> <a href="http://dev.geogebra.org/examples/html/example-popup.html">GeoGebra Classic in a Popup Dialog</a></li>'+
-	'<li> <a href="http://dev.geogebra.org/examples/html/example-api-listeners.html">Event listeners</a></li>'+
-	'<li> <a href="http://dev.geogebra.org/examples/html/example-api-sync.html">Communication between applets</a></li>'+
-	'<li> <a href="http://dev.geogebra.org/examples/html/example-exercise.html">Exercise</a></li></ul>'+
-	'<h4>Documentation</h4><ul><li><a href="https://www.geogebra.org/manual/en/Reference:Math_Apps_Embedding">Math Apps Embedding</a></li>'+
+	'<h2>GeoGebra Apps</h2><h4>Examples</h4><ul><li> <a href="example-single.html">Single applet</a></li>'+
+	'<li> <a href="example-3d.html">Single applet 3D</a></li>'+
+	'<li> <a href="example-multiple.html">Two applets</a></li>'+
+	'<li> <a href="example-api.html">Single applet + API</a></li>'+
+	'<li> <a href="example-api-multiple.html">Two applets + API</a></li>'+
+	'<li> <a href="example-api-save-state.html">Single applet + API for saving</a></li>'+
+    '<li> <a href="example-graphing.html">Graphing Calculator with Toolbar</a></li>'+
+    '<li> <a href="example-geometry.html">Geometry with Toolbar</a></li>'+
+    '<li> <a href="example-tools.html">GeoGebra Classic with Toolbar</a></li>'+
+    '<li> <a href="example-popup-graphing.html">Graphing Calculator in a Popup Dialog</a></li>'+
+    '<li> <a href="example-popup-geometry.html">Geometry in a Popup Dialog</a></li>'+
+    '<li> <a href="example-popup.html">GeoGebra Classic in a Popup Dialog</a></li>'+
+	'<li> <a href="example-api-listeners.html">Event listeners</a></li>'+
+	'<li> <a href="example-api-sync.html">Communication between applets</a></li>'+
+	'<li> <a href="example-exercise.html">Exercise</a></li></ul>'+
+	'<h4>Documentation</h4><ul><li><a href="https://www.geogebra.org/manual/en/Reference:GeoGebra_Apps_Embedding">GeoGebra Apps Embedding</a></li>'+
 	'<li><a href="https://www.geogebra.org/manual/en/Reference:JavaScript">JavaScript API</a></li></ul>';
 
 	var menuDiv = document.createElement('div');
