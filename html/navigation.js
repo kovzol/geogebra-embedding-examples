edc2ce704ca1f19fb6fef3da7f0ba9c6bd55c8c5
Zoltán Kovács <zoltan@geogebra.org>
2018-04-05 17:01:17 +0200

    Add Animation

--- html/navigation.js
+++ html/navigation.js
@@ -27,7 +27,9 @@ function addDrawer(){
     '<li> <a href="example-popup.html">GeoGebra Classic in a Popup Dialog</a></li>'+
 	'<li> <a href="example-api-listeners.html">Event listeners</a></li>'+
 	'<li> <a href="example-api-sync.html">Communication between applets</a></li>'+
-	'<li> <a href="example-exercise.html">Exercise</a></li></ul>'+
+	'<li> <a href="example-exercise.html">Exercise</a></li>'+
+	'<li> <a href="example-animation.html">Animation</a></li></ul>'+
+
 	'<h4>Documentation</h4><ul><li><a href="https://www.geogebra.org/manual/en/Reference:GeoGebra_Apps_Embedding">GeoGebra Apps Embedding</a></li>'+
 	'<li><a href="https://www.geogebra.org/manual/en/Reference:JavaScript">JavaScript API</a></li></ul>';
 
